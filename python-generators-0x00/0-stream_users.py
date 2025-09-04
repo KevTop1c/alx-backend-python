df58697055a926600f96db2993b207aea2152d39
Kevin <kev.topic001@gmail.com>
2025-09-04 11:50:26 +0000

    Add python script that creates a generator to stream rows from an SQL database one by one

---
+++ python-generators-0x00/0-stream_users.py
@@ -0,0 +1,28 @@
+import mysql.connector
+from contextlib import contextmanager
+from dotenv import load_dotenv
+import os
+
+# Load environment variable from .env
+load_dotenv()
+
+@contextmanager
+def get_connection():
+    connection = mysql.connector.connect(
+        host=os.getenv("DB_HOST", "localhost"),
+        user=os.getenv("DB_USER", "root"),
+        password=os.getenv("DB_PASSWORD", ""),
+        database=os.getenv("DB_NAME", "ALX_prodev"),
+    )
+    try:
+        yield connection
+    finally:
+        connection.close()
+
+
+def stream_users():
+    with get_connection() as connection:
+        with connection.cursor(dictionary=True) as cursor:
+            cursor.execute("SELECT * FROM user_data")
+            for row in cursor:
+                yield row
